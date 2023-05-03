168e0646b56493f2ecefb62356ad2c136ff2109f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-03 02:06:33 -0700

    install hugo during the first execution

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Install required packages
-apt-get update && apt-get install -y apt-get
-apt-get update && apt-get install -y curl make
-
-# Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-ARM64.tar.gz
-tar -xzf hugo_0.111.3_Linux-ARM64.tar.gz -C /usr/local/bin/
-rm hugo_0.111.3_Linux-ARM64.tar.gz
-export PATH=/usr/local/bin:$PATH
+# Install required packages, Hugo and Make
+apt-get update && apt-get install -y curl hugo make
 
 # Run make build
 make build
