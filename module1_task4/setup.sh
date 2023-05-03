5f615df34ec6a9f0173b3a1ab27b0a990813e240
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-03 02:12:52 -0700

    Update setup.sh with success message

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Install required packages, Hugo and Make
-apt-get update && apt-get install -y curl hugo make
+# Install required packages
+apt-get update && apt-get install -y curl make
+
+# Install Hugo
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-ARM64.tar.gz
+tar -xzf hugo_0.111.3_Linux-ARM64.tar.gz -C /usr/local/bin/
+rm hugo_0.111.3_Linux-ARM64.tar.gz
+export PATH=/usr/local/bin:$PATH
 
 # Run make build
 make build
+
+echo "The installation is successfully completed" >&2
