c97df7c87dbd76f82313ac981c401dddd98211f0
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-03 02:19:47 -0700

    change hugo version for the 0.84.0

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,9 +4,9 @@
 apt-get update && apt-get install -y curl make
 
 # Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-ARM64.tar.gz
-tar -xzf hugo_0.111.3_Linux-ARM64.tar.gz -C /usr/local/bin/
-rm hugo_0.111.3_Linux-ARM64.tar.gz
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
+rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 export PATH=/usr/local/bin:$PATH
 
 # Run make build
