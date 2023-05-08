9f4428976f441c3b21086d0406e08cf9b9c6fbf2
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-08 03:35:09 -0700

    uptade makefile and main integration test

--- module2_task3/main_integration_test.go
+++ module2_task3/main_integration_test.go
@@ -12,7 +12,6 @@ func Test_server(t *testing.T) {
   if testing.Short() {
     t.Skip("Flag `-short` provided: skipping Integration Tests.")
   }
-  
 
   tests := []struct {
     name         string
@@ -33,35 +32,24 @@ func Test_server(t *testing.T) {
       body:         "Hello Holberton!",
     },
     {
-      name:         "Health page",
+      name:         "Health Check",
       URI:          "/health",
       responseCode: 200,
       body:         "ALIVE",
     },
     {
-      name:         "Hello page",
-      URI:          "/hello?name=Holberton",
+      name:         "Hello without name parameter",
+      URI:          "/hello",
       responseCode: 200,
-      body:         "Hello Holberton!",
+      body:         "Hello there!",
     },
     {
-      name:         "Home page",
-      URI:          "",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
-    {
-      name:         "Hello page with empty name value",
+      name:         "Hello with empty name parameter",
       URI:          "/hello?name=",
       responseCode: 400,
       body:         "",
     },
-    {
-      name:         "Empty name value",
-      URI:          "/hello?name=",
-      responseCode: 400,
-      body:         "",
-    },
+    
   }
 
   for _, tt := range tests {
@@ -93,35 +81,5 @@ func Test_server(t *testing.T) {
         t.Errorf("handler returned unexpected body: got %q want %q", gotBody, expectedBody)
       }
     })
-  }
-}
-func Test_setupRouter(t *testing.T) {
-  r := setupRouter()
-
-  // Test that the /health endpoint returns a 200 status code
-  req, _ := http.NewRequest("GET", "/health", nil)
-  rr := httptest.NewRecorder()
-  r.ServeHTTP(rr, req)
-
-  if status := rr.Code; status != http.StatusOK {
-    t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-  }
-
-  // Test that the /hello endpoint returns a 400 status code with an empty name parameter
-  req, _ = http.NewRequest("GET", "/hello?name=", nil)
-  rr = httptest.NewRecorder()
-  r.ServeHTTP(rr, req)
-
-  if status := rr.Code; status != http.StatusBadRequest {
-    t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-  }
-
-  // Test that the /hello endpoint returns a 200 status code with a non-empty name parameter
-  req, _ = http.NewRequest("GET", "/hello?name=Holberton", nil)
-  rr = httptest.NewRecorder()
-  r.ServeHTTP(rr, req)
-
-  if status := rr.Code; status != http.StatusOK {
-    t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
   }
 }
