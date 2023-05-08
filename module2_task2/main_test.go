1417869a9954694bb307de022e6e8c241bf568d4
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-07 23:43:39 -0700

    add tests on file

--- module2_task2/main_test.go
+++ module2_task2/main_test.go
@@ -31,6 +31,18 @@ func Test_HelloHandler(t *testing.T) {
       body:         "Hello Rosalind Franklin!",
     },
     // INSERT MORE TESTS HERE
+    {
+      name:         "No name parameter",
+      queryString:  "",
+      responseCode: 200,
+      body:         "Hello there!",
+    },
+    {
+      name:         "Empty name value",
+      queryString:  "name=",
+      responseCode: 400,
+      body:         "",
+    },    
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
