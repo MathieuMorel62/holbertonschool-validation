64c2249d9501ee398c21bc06b2c5cce9c8dc80ff
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-19 21:03:19 +0200

    add mardown in stup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make
+apt-get update && apt-get install -y curl make npm
 
 # Install Hugo
 curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
 rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 export PATH=/usr/local/bin:$PATH
+
+# Install markdownlint-cli
+npm install -g markdownlint-cli markdown-link-check
 
 # Run make build
 make build
