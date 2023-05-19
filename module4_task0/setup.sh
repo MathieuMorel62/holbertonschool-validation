5084a1de3f6101b122b2d84d628c996d24b417e3
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-19 21:22:52 +0200

    update file

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make npm
+apt-get update && apt-get install -y curl make
 
 # Install Hugo
 curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
