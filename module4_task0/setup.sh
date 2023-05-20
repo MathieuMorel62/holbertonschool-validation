1d8d7e42fc95c8c7a57bc680e8e92631cad14008
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 16:07:19 +0200

    update workflows

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf /usr/local/go
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt-get install ./install_hugo.deb
+# Install required packages
+apt-get update && apt-get install -y curl make npm wget 
+
+# Install Hugo
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz
