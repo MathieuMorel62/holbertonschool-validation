3419b8c76df8e5fc8411d6450a3b9128e41fe6a5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 14:52:26 +0200

    change hugo format

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y git curl make wget build-essential
+sudo apt-get update && sudo apt-get install -y git curl make wget build-essential
 
-# Install Hugo
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.deb
-dpkg -xzf hugo_extended_0.109.0_Linux-amd64.deb
+# Download Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+# Extract the file
+tar -xzf hugo_extended_0.109.0_Linux-64bit.tar.gz
