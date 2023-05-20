e2a95c845d173cb444da75888816678dc9018451
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 14:49:22 +0200

    change hugo version

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,5 +4,5 @@
 apt-get update && apt-get install -y git curl make wget build-essential
 
 # Install Hugo
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.deb
 dpkg -xzf hugo_extended_0.109.0_Linux-amd64.deb
