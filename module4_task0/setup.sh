2a028a714738bb6ba1dcc10507f54d5d8b63845b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 14:46:14 +0200

    update workflows and setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make
+apt-get update && apt-get install -y git curl make wget build-essential
 
 # Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
-rm hugo_extended_0.84.0_Linux-64bit.tar.gz
-export PATH=/usr/local/bin:$PATH
-
-# Run make build
-make build
-
-echo "The installation is successfully completed" >&2
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+dpkg -xzf hugo_extended_0.109.0_Linux-amd64.deb
