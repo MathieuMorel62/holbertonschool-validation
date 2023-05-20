1b86adc519553b57a71481f6c6239a9ef421ab76
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 15:58:56 +0200

    update workflows and setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install Hugo
 rm -rf /usr/local/go
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
-
-echo "The installation is successfully completed" >&2
