f10dcc3ff3c6c3e363d1bda4f1fcdece42ac2dfe
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-20 16:12:57 +0200

    update workflows

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
-# Install required packages
-apt-get update && apt-get install -y curl make npm wget 
-
-# Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo apt-get update && sudo apt-get install -y make git zip
+# download/install minimal version to use template ananke
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+#remove file after installation
+sudo rm hugo.deb
+# download/install linter
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+# Uninstall go
+sudo apt-get remove golang-go
+rm -rf /usr/local/go
+# remove dist directory
+rm -rf dist/ 2> /dev/null
