8e048a2cd22b28b60dc27e8a2fc1ce29148badc8
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 15:30:19 +0200

    update makefile

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update package lists and installation of required packages
-apt-get update && apt-get install -y hugo make npm curl
+apt-get update && apt-get install -y hugo make npm
 
 # download minimal version to use 'hugo' with the template ananke
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
@@ -12,7 +12,7 @@ apt install ./hugo.deb
 # remove file after the installation
 rm hugo.deb
 
-# Running the command make build to build the website
+# Running the command `make build` to build the website
 make build
 
 # Install markdownlint-cli and markdown-link-check
