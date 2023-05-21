0aaf6368c0f6257e2ff547a59014ea2be57327a5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 13:12:15 +0200

    add new path

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -6,6 +6,7 @@ sudo apt-get update && sudo apt-get install -y curl make git zip wget npm
 # Install Hugo
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
+export PATH=/usr/local/bin:$PATH
 
 #remove file after installation
 sudo rm hugo.deb
