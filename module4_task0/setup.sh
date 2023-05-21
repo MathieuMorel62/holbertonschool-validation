f0a79e76af2596b640a8bbcb89f523f8b4dd42f2
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 14:38:25 +0200

    change stup and workflows

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make
+sudo apt-get update && apt-get install -y curl make wget npm
 
 # Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
-rm hugo_extended_0.84.0_Linux-64bit.tar.gz
-export PATH=/usr/local/bin:$PATH
+sudo wget https://github.com/gohugoio/hugo/releases/download/v0.87.0/hugo_extended_0.87.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.87.0_Linux-64bit.deb
 
-# Run make build
-make build
-
-echo "The installation is successfully completed" >&2
+sudo npm install -g markdownlint-cli markdown-link-check
