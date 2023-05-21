905c4faa96b302d7b2390b8772c9d926d9cfddec
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 11:53:10 +0200

    add sudo before apt-get

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make git zip wget npm
+sudo apt-get update && sudo apt-get install -y curl make git zip wget npm
 
 # Install Hugo
 sudo curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
 sudo rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 
-
 # Uninstall go
 sudo apt-get remove golang-go
-rm -rf /usr/local/go
+sudo rm -rf /usr/local/go
 
 # remove dist directory
-rm -rf dist/ 2> /dev/null
+sudo rm -rf dist/ 2> /dev/null
