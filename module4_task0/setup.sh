34aeb9bf4f8041308449124e75c86af2c30d5d8c
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 12:11:16 +0200

    add export path

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -7,6 +7,7 @@ sudo apt-get update && sudo apt-get install -y curl make git zip wget npm
 sudo curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
 sudo rm hugo_extended_0.84.0_Linux-64bit.tar.gz
+export PATH=/usr/local/bin:$PATH
 
 # Uninstall go
 sudo apt-get remove golang-go
