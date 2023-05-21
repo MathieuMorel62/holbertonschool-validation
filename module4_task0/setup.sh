84b554127b044c45f2350889946532df79b07739
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-21 11:17:20 +0200

    install markdown and delete go

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Install required packages
-apt-get update && apt-get install -y curl make npm
+apt-get update && apt-get install -y curl make git zip wget npm
 
 # Install Hugo
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo_extended_0.84.0_Linux-64bit.tar.gz -C /usr/local/bin/
-rm hugo_extended_0.84.0_Linux-64bit.tar.gz
-export PATH=/usr/local/bin:$PATH
+sudo rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 
-# Run make build
-make build
+# download/install linter
 
-echo "The installation is successfully completed" >&2
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+
+# Uninstall go
+sudo apt-get remove golang-go
+rm -rf /usr/local/go
+
+# remove dist directory
+rm -rf dist/ 2> /dev/null
