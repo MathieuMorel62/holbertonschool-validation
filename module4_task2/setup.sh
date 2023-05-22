98274c64fbd5621c0228fa489ca592218bcae973
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-22 18:04:39 +0200

    add sudo for install package to septup.sh

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Update package lists and installation of required packages
-apt-get update && apt-get install -y hugo make npm curl wget
+sudo apt-get update && apt-get install -y hugo make npm curl wget
