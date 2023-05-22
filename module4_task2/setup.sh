8fcab58945d253a3280f981fa0d63c5874b58dc8
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-22 18:35:28 +0200

    add hugo install to workflows

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update package lists and installation of required packages
-apt-get update && apt-get install -y hugo make npm curl wget
+apt-get update && apt-get install -y hugo make npm curl
 
 # download minimal version to use hugo with the template ananke
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
