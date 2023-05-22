9d674b0f44505daae9c467979ce6249ffa40682b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-05-22 13:07:49 +0200

    add link for install hadolint and container struct

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update package lists and installation of required packages
-apt-get update && apt-get install -y hugo make npm curl
+apt-get update && apt-get install -y hugo make npm curl wget
 
 # download minimal version to use hugo with the template ananke
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
@@ -15,3 +15,13 @@ rm hugo.deb
 # Install markdownlint-cli and markdown-link-check
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
+
+# Install hadolint
+wget -q -O /usr/local/bin/hadolint \
+"https://github.com/hadolint/hadolint/releases/download/v2.6.0/hadolint-Linux-x86_64"
+chmod +x /usr/local/bin/hadolint
+
+# Install Container Structure Test
+wget -q -O /usr/local/bin/container-structure-test \
+"https://storage.googleapis.com/container-structure-test/v1.10.0/container-structure-test-linux-amd64"
+chmod +x /usr/local/bin/container-structure-test
